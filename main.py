aa06c30094b0be3a3796d45d6085c623360cc02c
bsim-work <bsimwork22@gmail.com>
2025-01-06 16:27:53 -0800

    added count_of_words function, which counts words

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 def main():
     file_path ="books/frankenstein.txt"
     text = get_book_contents(file_path)
-    print(text)
+    word_count = count_of_words(text)
+    print(word_count)
 
 def get_book_contents(file_path):
     with open(file_path) as f:
         return f.read()
 
-main()    
+def count_of_words(text):
+    word_count = len(text.split())
+    return word_count
+
+main()

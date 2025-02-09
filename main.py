60edb384a028ea841bc24083cf148ebc35d6ffab
bsim-work <bsimwork22@gmail.com>
2025-02-09 13:34:57 -0800

    added a print report function that will print total words and each alphabetical character and occurence

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ def main():
     lower_case = text.lower()
     #print(lower_case)
     character_counter = count_characters(lower_case)
-    print(character_counter)
-
+    #print(character_counter)
+    print_report(word_count, character_counter)
 
 def get_book_contents(file_path):
     with open(file_path) as f:
@@ -32,5 +32,17 @@ def count_characters(lower_case):
         characters[key] = lower_case.count(key)
 
     return characters
+
+def print_report(word_count, character_counter):
+
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{word_count} words found in the document")
+
+    for key in character_counter.keys():
+        if key.isalpha() == True:
+            value = character_counter.get(key)
+            print (f"The '{key}' character was found {value} times")
+
+    print("--- End report ---")
 
 main()
